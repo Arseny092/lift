d03735f31e5e910392145a8af49a7e7e83c265a5
Arseny <arsenyparamonov@gmail.com>
2018-09-25 11:52:51 +0300

    add Parallax
    Tested with all screen resolutions

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,12 +2,14 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
+  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/arseny/Documents/lift/.cache/dev-404-page.js"),
   "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/arseny/Documents/lift/src/pages/404.js"),
   "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/arseny/Documents/lift/src/pages/index.js")
 }
 
 exports.json = {
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/arseny/Documents/lift/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/arseny/Documents/lift/.cache/json/dev-404-page.json"),
   "404.json": require("gatsby-module-loader?name=path---404!/Users/arseny/Documents/lift/.cache/json/404.json"),
   "index.json": require("gatsby-module-loader?name=path---index!/Users/arseny/Documents/lift/.cache/json/index.json"),
   "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/arseny/Documents/lift/.cache/json/404-html.json")
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,12 +7,14 @@ exports.layouts = {
 }
 
 exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/arseny/Documents/lift/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/arseny/Documents/lift/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/arseny/Documents/lift/src/pages/index.js"))
 }
 
 exports.json = {
   "layout-index.json": require("/Users/arseny/Documents/lift/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("/Users/arseny/Documents/lift/.cache/json/dev-404-page.json"),
   "404.json": require("/Users/arseny/Documents/lift/.cache/json/404.json"),
   "index.json": require("/Users/arseny/Documents/lift/.cache/json/index.json"),
   "404-html.json": require("/Users/arseny/Documents/lift/.cache/json/404-html.json")
